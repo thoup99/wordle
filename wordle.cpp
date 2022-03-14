123a84da451661d55a7b4f7bd875399926855241
thoup99 <45801047+thoup99@users.noreply.github.com>
2022-03-13 20:57:42 -0400

    Add files via upload

--- wordle.cpp
+++ wordle.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include <string>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 class Console
 {
@@ -10,7 +12,7 @@ class Console
     public:
         void printColor();
         void changeColor();
-        
+
 
     void printColor(char letter, std::string colorName)
     {
@@ -35,12 +37,72 @@ class Console
     }
 };
 
+boolean binarySearch(std::string arr[], int start, int end, std::string value){
+    int high = end;
+    int low = start;
+    while (low <= high)
+    {
+        int mid = (high + low) / 2;
+        if (arr[mid] == value)
+        {
+            return true;
+        }
+        else
+        {
+            if (value.compare(arr[mid]) < 0)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+    }
+    return false;
+}
+
 
 int main() {
-    Console c = Console();
+    Console console = Console();
     std::fstream myFile;
 
-    std::string word = "words";
+    //Load word list
+    std::string word_list[2315];
+    myFile.open("word_list.txt", std::ios::in);
+    if (myFile.is_open())
+    {
+        int index = 0;
+        std::string temp;
+        while (std::getline(myFile, temp))
+        {
+            word_list[index] = temp;
+            index++;
+        }
+        myFile.close();
+    }
+
+    //load allowed list
+    std::string allowed_list[10657];
+    myFile.open("allowed_list.txt", std::ios::in);
+    if (myFile.is_open())
+    {
+        int index = 0;
+        std::string temp;
+        while (std::getline(myFile, temp))
+        {
+            allowed_list[index] = temp;
+            index++;
+        }
+        myFile.close();
+    }
+
+    //Picks a random word from the word list
+    srand(time(0));
+    int randIndex = rand() % 2315 + 1;
+    std::string word = word_list[randIndex];
+
+    //Writes the current word to a file
     myFile.open("answer.txt", std::ios::out);
     if (myFile.is_open())
     {
@@ -55,22 +117,39 @@ int main() {
     for (int attempt = 1; attempt <= 5; attempt++)
     {
         //Gets the Users guess
+        boolean isValidGuess = false;
         std::string guess;
-        std::cout << "Attempt " << attempt << ". Please enter a 5 letter word.\n";
-        std::cin >> guess;
-        while (guess.length() != 5)
+
+        std::cout << "Attempt " << attempt << ". ";
+        while (!isValidGuess)
         {
-            std::cout << "Please enter a 5 letter word." << std::endl;
-            std::cin >> guess;
+            
+            while (guess.length() != 5)
+            {
+                std::cout << "Please enter a 5 letter word." << std::endl;
+                std::cin >> guess;
+            }
+            if (!(binarySearch(word_list, 0, 2314, guess) or binarySearch(allowed_list, 0, 10656, guess)))
+            {
+                std::cout << "That word is not in the word list. ";
+                guess = "";
+                continue;
+            }
+            isValidGuess = true;
         }
+        
+
+        //Goes back one line in the terminal and then backspaces 5 times.
+        std::cout << "\033[F" << "\b\b\b\b\b";
         
         //Checks each letter of the guess
         for (int i = 0; i <= 4; i++){
-            char letter = guess.at(i);
+            char letter = std::tolower(guess.at(i));
+            guess.at(i) = letter; //coverts the guess to lower case
 
             //Letter is in the correct spot
             if (word.at(i) == letter){
-                c.printColor(letter, "green");
+                console.printColor(letter, "green");
                 continue;
             }
 
@@ -93,7 +172,7 @@ int main() {
             //letter does not appear at all
             if (appeared == 0) 
             {
-                c.printColor(letter, "grey");
+                console.printColor(letter, "grey");
             }
 
 
@@ -102,29 +181,30 @@ int main() {
             {
                 if (appearedCorrect < appeared)
                 {
-                    c.printColor(letter, "yellow");   
+                    console.printColor(letter, "yellow");   
                 }
                 else
                 {
-                    c.printColor(letter, "grey");
+                    console.printColor(letter, "grey");
                 }
-                //std::cout << appeared << " " <<appearedCorrect << " " << appearedInGuess << std::endl;
             }
         
         }
         std::cout << "" <<std::endl;
-        c.setColor("white");
+        console.setColor("white");
         if (word == guess)
         {
-            std::cout << "You guessed the word correct in " << attempt << " tries!";
+            std::cout << "You guessed the word correct in " << attempt << " tries!" << std::endl;
             isRight = true;
             break;
         }
     }
     if (!isRight)
     {
-        c.setColor("white");
+        console.setColor("white");
         std::cout << "You did not guess the word properly. It was " << word << "." << std::endl;
     }
-    std::cin.get();
+    std::string end;
+    std::cout << "Type and press enter to close." << std::endl;
+    std::cin >> end;
 }
